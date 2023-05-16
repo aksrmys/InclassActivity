8d980f2c28c2dff7a51323d66c5ccf753600f681
Rümeysa Akış <80596533+aksrmys@users.noreply.github.com>
2023-05-17 00:03:09 +0300

    Add files via upload

---
+++ [Akış]-Activity8/Activity8.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    var programmingLanguages = [
+        "ActionScript","AppleScript","Asp","JavaScript", "Lisp", "Perl", "PHP", "Python"
+    ];
+    $("#programming-language").autocomplete({
+      source: programmingLanguages
+    });
+    
+    $("#birthday").datepicker();
+  });
